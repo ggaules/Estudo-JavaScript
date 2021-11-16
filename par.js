4aeaf03335088a2f438c903aebe219643b02d5a6
Gabriel Lopes <ggauless@gmail.com>
2021-11-16 16:17:00 -0400

    gabriel

--- par.js
+++ par.js
@@ -2,3 +2,25 @@
 
 const saudacao = "opa"
 console.log(saudacao)
+
+function exec() {
+    const saudacao = "Falaaa" 
+    return saudacao
+
+}
+
+// objetos são grupos alinhados de pares/ valor
+
+const cliente = {
+    nome: 'PEdro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        lagradouro:'Rua muito larga'
+        
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
---
+++ usandovar2.js
@@ -0,0 +1,6 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('fora =', numero)
+}
+console.log('fora =', numero)
