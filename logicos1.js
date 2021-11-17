7144b31213c03f5d48e47f4af20b97d41209638e
Gabriel Lopes <ggauless@gmail.com>
2021-11-17 16:43:05 -0400

    a

---
+++ logicos1.js
@@ -0,0 +1,7 @@
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    //const comprarTv32 = !!(trabalho1 ^ trabalho2)
+    const comprarTv32 = trabalho1 != trabalho2 
+
+}
