78969983eed855dfd82fb81b6f3b54dc05ffc959
Gabriel Lopes <ggauless@gmail.com>
2021-11-18 16:31:09 -0400

    a

---
+++ erro.js
@@ -0,0 +1,22 @@
+function imprimirNoemGritando(obj) {
+    throw new Error('...')
+    throw 10
+    throw true
+    throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new date
+    }
+    
+    try {
+        console.log(obj.name.toUpperCase() + '!!!!')
+    } catch (e) {
+        tratarErroeLancar(e)
+        finally {
+            console.log('final')
+        }
+    }
+
+const obj = { nome: 'Roberto'}
+imprimirNoemGritando(obj)
